ae437a8934d8dcfcbfa75fa7255df3c93da10e4d
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-11-10 10:38:30 -0400

    add zod and extend bundler core

--- packages/bundler-plugin-core/src/index.ts
+++ packages/bundler-plugin-core/src/index.ts
@@ -1,7 +1,64 @@
-import { createUnplugin } from "unplugin";
+import { type UnpluginOptions, createUnplugin } from "unplugin";
+import {
+  type BundleAnalysisUploadPlugin,
+  type Asset,
+  type Chunk,
+  type Module,
+  type Options,
+  type Output,
+} from "./types.js";
 
-export const unplugin = createUnplugin((_options) => {
-  return {
-    name: "unplugin-stats",
-  };
-});
+import { jsonSchema } from "./schemas.js";
+import { z } from "zod";
+interface CodecovUnpluginFactoryOptions {
+  bundleAnalysisUploadPlugin: BundleAnalysisUploadPlugin;
+}
+
+export function codecovUnpluginFactory({
+  bundleAnalysisUploadPlugin,
+}: CodecovUnpluginFactoryOptions) {
+  return createUnplugin<Options, true>((userOptions, _unpluginMetaContext) => {
+    const plugins: UnpluginOptions[] = [];
+
+    if (userOptions?.enableBundleAnalysis) {
+      const statsFileName = z
+        .string()
+        .endsWith(".json")
+        .optional()
+        .parse(userOptions?.statsFileName);
+
+      const output: Output = {
+        version: "1",
+      };
+
+      let startTime = NaN;
+      const { pluginVersion, version, ...pluginOpts } =
+        bundleAnalysisUploadPlugin({
+          output,
+          statsFileName,
+        });
+
+      plugins.push({
+        ...pluginOpts,
+        buildStart() {
+          startTime = Date.now();
+          output.version = version;
+          output.plugin = {
+            name: pluginOpts.name,
+            version: pluginVersion,
+          };
+          output.builtAt = startTime;
+        },
+        buildEnd(this) {
+          const duration = Date.now() - startTime;
+          output.duration = duration;
+        },
+      });
+    }
+
+    return plugins;
+  });
+}
+
+export type { BundleAnalysisUploadPlugin, Asset, Chunk, Module, Options };
+export { jsonSchema };
