d5c478d2216a75d6739f42384fd92275c137cce2
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-11-10 10:37:54 -0400

    expand vite bundler example a little bit

--- bundlers/vite/src/App.tsx
+++ bundlers/vite/src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, lazy, Suspense } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const IndexedLazyComponent = lazy(() => import("./IndexedLazyComponent"));
+const LazyComponent = lazy(() => import("./LazyComponent/LazyComponent"));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +31,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Suspense fallback={null}>
+        <IndexedLazyComponent />
+      </Suspense>
+      <Suspense fallback={null}>
+        <LazyComponent />
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- bundlers/vite/vite.config.ts
+++ bundlers/vite/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStatsPlugin } from "@codecov/vite-plugin";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteStatsPlugin()],
+  plugins: [react(), codecovVitePlugin({ enableBundleAnalysis: true })],
 });
