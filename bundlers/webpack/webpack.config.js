25b7c8e10140bfcb1de633c22b4ba055a45d4c01
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-11-17 10:46:20 -0400

    setup plain webpack bundler to work with plugin

--- bundlers/webpack/webpack.config.js
+++ bundlers/webpack/webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
+const { codecovWebpackPlugin } = require("@codecov/webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  mode: 'production',
+  mode: "production",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    codecovWebpackPlugin({
+      enableBundleAnalysis: true,
+    }),
+  ],
 };
