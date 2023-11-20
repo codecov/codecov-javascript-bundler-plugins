ecffeb1652a1509587fe593c3315a0f12a60b999
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-11-20 13:28:19 -0400

    remove pre-node 18 upload and request functions as we will only support node 18 and greater

--- packages/bundler-plugin-core/src/utils/getPreSignedURL.ts
+++ packages/bundler-plugin-core/src/utils/getPreSignedURL.ts
@@ -1,4 +1,3 @@
-import https from "https";
 import { z } from "zod";
 
 interface GetPreSignedURLArgs {
@@ -15,56 +14,24 @@ export const getPreSignedURL =
   ({ apiURL, globalUploadToken, repoToken }: GetPreSignedURLArgs) =>
   async () => {
     const token = globalUploadToken ?? repoToken;
-    const url = `http://${apiURL}/upload/service/commits/sha/bundle_analysis`;
+    const commitSha = "";
+    const url = `http://${apiURL}/upload/service/commits/${commitSha}/bundle_analysis`;
 
-    if (typeof fetch !== "undefined") {
-      try {
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `token ${token}`,
-          },
-        });
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `token ${token}`,
+        },
+      });
 
-        const data = await response.json();
-        const parsedData = PreSignedURLSchema.parse(data);
+      const data = await response.json();
+      const parsedData = PreSignedURLSchema.parse(data);
 
-        return parsedData.url;
-      } catch (e) {
-        console.error(e);
-      }
-    } else {
-      let presignedURL = "";
-      const urlObj = new URL(url);
-
-      try {
-        const req = https.request(
-          {
-            hostname: urlObj.hostname,
-            path: urlObj.pathname,
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `token ${token}`,
-            },
-          },
-          (res) => {
-            let data = "";
-            res.on("data", (chunk) => {
-              data += chunk;
-            });
-            res.on("end", () => {
-              presignedURL = data;
-            });
-          },
-        );
-        req.end();
-
-        return presignedURL;
-      } catch (e) {
-        console.error(e);
-      }
+      return parsedData.url;
+    } catch (e) {
+      console.error(e);
     }
 
     return;
--- packages/bundler-plugin-core/src/utils/handleUpload.ts
+++ packages/bundler-plugin-core/src/utils/handleUpload.ts
@@ -1,74 +1,14 @@
 import { ReadableStream, TextEncoderStream } from "node:stream/web";
-import https from "https";
 
-function supportsRequestStreams() {
-  let duplexAccessed = false;
-
-  // early return if Request is not available
-  if (typeof Request === "undefined") {
-    return false;
-  }
-
-  const hasContentType = new Request("http://127.0.0.1", {
-    // @ts-expect-error checking to see if the ReadableStream is sent
-    // to a toString value which indicates that streams are not
-    // supported in the running version of node
-    body: new ReadableStream(),
-    method: "PUT",
-    get duplex() {
-      duplexAccessed = true;
-      return "half";
-    },
-  }).headers.has("Content-type");
-
-  return duplexAccessed && !hasContentType;
-}
-
-interface HandlePreFetchUploadArgs {
+interface HandleUploadArgs {
   message: string;
   preSignedUrl: string;
 }
 
-function handlePreFetchUpload({
+export async function handleUpload({
   message,
   preSignedUrl,
-}: HandlePreFetchUploadArgs) {
-  const url = new URL(preSignedUrl);
-
-  try {
-    const req = https.request(
-      {
-        hostname: url.hostname,
-        path: url.pathname,
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-      },
-      (res) => {
-        res.on("data", () => null);
-        res.on("end", () => {
-          console.debug("\nStatus code:", res.statusCode);
-        });
-      },
-    );
-
-    req.write(message);
-    req.end();
-  } catch (e) {
-    console.debug("\n", e);
-  }
-}
-
-interface HandleFetchUploadArgs {
-  message: string;
-  preSignedUrl: string;
-}
-
-async function handleFetchUpload({
-  message,
-  preSignedUrl,
-}: HandleFetchUploadArgs) {
+}: HandleUploadArgs) {
   const iterator = message[Symbol.iterator]();
   const stream = new ReadableStream({
     pull(controller) {
@@ -94,24 +34,10 @@ async function handleFetchUpload({
       body: stream,
     });
 
-    console.debug("\n response: ", response.ok);
+    return response;
   } catch (e) {
     console.error(e);
   }
-}
 
-interface HandleUploadArgs {
-  message: string;
-  preSignedUrl: string;
-}
-
-export async function handleUpload({
-  message,
-  preSignedUrl,
-}: HandleUploadArgs) {
-  if (supportsRequestStreams()) {
-    await handleFetchUpload({ message, preSignedUrl });
-  } else {
-    handlePreFetchUpload({ message, preSignedUrl });
-  }
+  return;
 }
