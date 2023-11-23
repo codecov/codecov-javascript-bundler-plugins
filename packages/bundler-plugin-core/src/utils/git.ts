0589f08cea359d2995f2b9ebb452372f04dfd396
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-11-23 13:11:08 -0400

    address feedback

--- packages/bundler-plugin-core/src/utils/git.ts
+++ packages/bundler-plugin-core/src/utils/git.ts
@@ -1,38 +1,42 @@
 import { runExternalProgram } from "./runExternalProgram.ts";
 
-export function parseSlug(slug: string): string {
+export function parseSlug(slug: unknown): string {
   // origin    https://github.com/torvalds/linux.git (fetch)
   // git@github.com: codecov / uploader.git
   if (typeof slug !== "string") {
     return "";
   }
 
-  if (slug.match("http:") ?? slug.match("https:") ?? slug.match("ssh:")) {
+  if (slug.match(/^(ssh|https?):/)) {
     // Type is http(s) or ssh
     const phaseOne = slug.split("//")[1]?.replace(".git", "") ?? "";
     const phaseTwo = phaseOne?.split("/") ?? "";
     const cleanSlug =
       phaseTwo.length > 2 ? `${phaseTwo[1]}/${phaseTwo[2]}` : "";
     return cleanSlug;
-  } else if (slug.match("@")) {
+  }
+
+  if (slug.match("@")) {
     // Type is git
     const cleanSlug = slug.split(":")[1]?.replace(".git", "");
     return cleanSlug ?? "";
   }
+
   throw new Error(`Unable to parse slug URL: ${slug}`);
 }
 
 export function parseSlugFromRemoteAddr(remoteAddr?: string): string {
-  let slug = "";
-  if (!remoteAddr) {
-    remoteAddr =
-      runExternalProgram("git", ["config", "--get", "remote.origin.url"]) ?? "";
+  const addr =
+    remoteAddr ??
+    runExternalProgram("git", ["config", "--get", "remote.origin.url"]) ??
+    "";
+  if (!addr) {
+    return "";
   }
-  if (remoteAddr) {
-    slug = parseSlug(remoteAddr);
-  }
+
+  const slug = parseSlug(addr);
   if (slug === "/") {
-    slug = "";
+    return "";
   }
   return slug;
 }
