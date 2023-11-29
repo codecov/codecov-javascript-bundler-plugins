f305229e291afb029fabd9847cbec4b87fce5aac
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-11-29 07:57:25 -0400

    add in new provider function to grab the current provider

--- packages/bundler-plugin-core/src/utils/provider.ts
+++ packages/bundler-plugin-core/src/utils/provider.ts
@@ -1,3 +1,24 @@
+import {
+  type ProviderServiceParams,
+  type ProviderUtilInputs,
+} from "../types.ts";
+import { cyan, red } from "./logging.ts";
+import { providerList } from "./providers";
+
+export async function detectProvider(
+  inputs: ProviderUtilInputs,
+): Promise<ProviderServiceParams> {
+  cyan("Detecting CI provider");
+  for (const provider of providerList) {
+    if (provider.detect(inputs.envs)) {
+      cyan(`Detected CI provider: ${provider.getServiceName()}`);
+      return await provider.getServiceParams(inputs);
+    }
+  }
+  red("Could not detect CI provider");
+  throw new Error("Could not detect provider");
+}
+
 export function setSlug(
   slugArg: string | undefined,
   orgEnv: string | undefined,
