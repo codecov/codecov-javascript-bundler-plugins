bcf8d637c74ffe9485aaee2a1fa461049711da2f
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-12-01 07:44:04 -0400

    throw an error if no commit is found

--- packages/bundler-plugin-core/src/utils/getPreSignedURL.ts
+++ packages/bundler-plugin-core/src/utils/getPreSignedURL.ts
@@ -8,6 +8,7 @@ import { DEFAULT_RETRY_COUNT } from "./constants.ts";
 import { fetchWithRetry } from "./fetchWithRetry.ts";
 import { green, red, yellow } from "./logging.ts";
 import { preProcessBody } from "./preProcessBody.ts";
+import { NoCommitShaError } from "../errors/NoCommitShaError.ts";
 
 interface GetPreSignedURLArgs {
   apiURL: string;
@@ -34,10 +35,13 @@ export const getPreSignedURL = async ({
     throw new NoUploadTokenError("No upload token found");
   }
 
-  const commitSha = serviceParams?.commit ?? "";
-  const url = `${apiURL}/upload/service/commits/${commitSha}/bundle_analysis`;
+  if (!serviceParams?.commit) {
+    red("No commit found");
+    throw new NoCommitShaError("No commit found");
+  }
 
-  const sentServiceParams = preProcessBody({ token, ...serviceParams });
+  const commitSha = serviceParams?.commit;
+  const url = `${apiURL}/upload/service/commits/${commitSha}/bundle_analysis`;
 
   let response: Response;
   try {
@@ -51,7 +55,7 @@ export const getPreSignedURL = async ({
           "Content-Type": "application/json",
           Authorization: `token ${token}`,
         },
-        body: JSON.stringify(sentServiceParams),
+        body: JSON.stringify(preProcessBody(serviceParams)),
       },
     });
   } catch (e) {
