fea569b9a13902aacca7919106b8d36e9db20991
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-12-05 13:03:37 -0400

    actually set bundler information

--- packages/webpack-plugin/src/webpack-bundle-analysis/webpackBundleAnalysisPlugin.ts
+++ packages/webpack-plugin/src/webpack-bundle-analysis/webpackBundleAnalysisPlugin.ts
@@ -85,6 +85,8 @@ export const webpackBundleAnalysisPlugin: BundleAnalysisUploadPlugin = ({
             });
           }
 
+          console.info("webpackBundleAnalysisPlugin output", output);
+
           // only output file if running dry run
           if (userOptions?.dryRun) {
             const { RawSource } = webpack4or5.sources;
