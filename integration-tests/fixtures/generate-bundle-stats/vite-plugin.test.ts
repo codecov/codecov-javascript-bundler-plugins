9e060bfbdd773a7116eaf0043d459918026a4f5c
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-03 08:05:44 -0400

    add vite v4 tests

--- integration-tests/fixtures/generate-bundle-stats/vite-plugin.test.ts
+++ integration-tests/fixtures/generate-bundle-stats/vite-plugin.test.ts
@@ -3,10 +3,44 @@
 import path from "path";
 import fs from "fs";
 import { type Output } from "@codecov/bundler-plugin-core";
-import { build } from "vite";
+import { build as viteV4 } from "viteV4";
+import { build as viteV5 } from "viteV5";
 import { codecovVitePlugin } from "@codecov/vite-plugin";
 
-const expectedStats = {
+const expectedV4Stats = {
+  version: "1",
+  plugin: { name: "codecov-vite-bundle-analysis-plugin", version: "1.0.0" },
+  builtAt: 1701788687217,
+  duration: 7,
+  bundler: { name: "rollup", version: "3.29.4" },
+  assets: [{ name: "main-Kc6Ge1DG.js", size: 216 }],
+  chunks: [
+    {
+      id: "main",
+      uniqueId: "0-main",
+      entry: true,
+      initial: false,
+      files: ["main-Kc6Ge1DG.js"],
+      names: ["main"],
+    },
+  ],
+  modules: [
+    {
+      name: "./src/getRandomNumber.js",
+      size: 98,
+      chunks: ["main"],
+      chunkUniqueIds: ["0-main"],
+    },
+    {
+      name: "./src/main.js",
+      size: 115,
+      chunks: ["main"],
+      chunkUniqueIds: ["0-main"],
+    },
+  ],
+};
+
+const expectedV5Stats = {
   version: "1",
   plugin: { name: "codecov-vite-bundle-analysis-plugin", version: "1.0.0" },
   builtAt: 1701788687217,
@@ -40,60 +74,121 @@ const expectedStats = {
 };
 
 describe("Generating vite stats", () => {
-  let stats: Output;
-  const vitePath = path.resolve(__dirname, "../../test-apps/vite");
-  beforeAll(async () => {
-    await build({
-      clearScreen: false,
-      root: vitePath,
-      build: {
-        outDir: "dist",
-        rollupOptions: {
-          input: `${vitePath}/index.html`,
-          output: {
-            format: "cjs",
+  describe("version 4", () => {
+    let stats: Output;
+    const vitePath = path.resolve(__dirname, "../../test-apps/vite");
+    beforeAll(async () => {
+      await viteV4({
+        clearScreen: false,
+        root: vitePath,
+        build: {
+          outDir: "dist",
+          rollupOptions: {
+            input: `${vitePath}/index.html`,
+            output: {
+              format: "cjs",
+            },
           },
         },
-      },
-      plugins: [
-        codecovVitePlugin({
-          enableBundleAnalysis: true,
-          dryRun: true,
-          bundleName: "vite-test",
-        }),
-      ],
+        plugins: [
+          codecovVitePlugin({
+            enableBundleAnalysis: true,
+            dryRun: true,
+            bundleName: "vite-test",
+          }),
+        ],
+      });
+
+      const statsFilePath = path.resolve(
+        vitePath,
+        "dist/codecov-bundle-stats.json",
+      );
+
+      const statsData = fs.readFileSync(statsFilePath);
+      stats = JSON.parse(statsData.toString()) as Output;
     });
 
-    const statsFilePath = path.resolve(
-      vitePath,
-      "dist/codecov-bundle-stats.json",
-    );
+    afterAll(() => {
+      fs.rm(
+        path.resolve(vitePath, "dist"),
+        { recursive: true, force: true },
+        () => null,
+      );
+    });
 
-    const statsData = fs.readFileSync(statsFilePath);
-    stats = JSON.parse(statsData.toString()) as Output;
+    it("sets the correct version", () => {
+      expect(stats.version).toStrictEqual(expectedV4Stats.version);
+    });
+
+    it("sets the correct plugin information", () => {
+      expect(stats.plugin).toStrictEqual(expectedV4Stats.plugin);
+    });
+
+    it("sets the correct bundler information", () => {
+      expect(stats.bundler).toStrictEqual(expectedV4Stats.bundler);
+    });
+
+    it("sets the correct bundle name", () => {
+      expect(stats.bundleName).toStrictEqual("vite-test-cjs");
+    });
   });
 
-  afterAll(() => {
-    fs.rm(
-      path.resolve(vitePath, "dist"),
-      { recursive: true, force: true },
-      () => null,
-    );
-  });
+  describe("version 5", () => {
+    let stats: Output;
+    const vitePath = path.resolve(__dirname, "../../test-apps/vite");
+    beforeAll(async () => {
+      await viteV5({
+        clearScreen: false,
+        root: vitePath,
+        build: {
+          outDir: "dist",
+          rollupOptions: {
+            input: `${vitePath}/index.html`,
+            output: {
+              format: "cjs",
+            },
+          },
+        },
+        plugins: [
+          codecovVitePlugin({
+            enableBundleAnalysis: true,
+            dryRun: true,
+            bundleName: "vite-test",
+          }),
+        ],
+      });
 
-  it("sets the correct version", () => {
-    expect(stats.version).toStrictEqual(expectedStats.version);
-  });
+      const statsFilePath = path.resolve(
+        vitePath,
+        "dist/codecov-bundle-stats.json",
+      );
 
-  it("sets the correct plugin information", () => {
-    expect(stats.plugin).toStrictEqual(expectedStats.plugin);
-  });
+      const statsData = fs.readFileSync(statsFilePath);
+      stats = JSON.parse(statsData.toString()) as Output;
+    });
 
-  it("sets the correct bundler information", () => {
-    expect(stats.bundler).toStrictEqual(expectedStats.bundler);
-  });
+    afterAll(() => {
+      fs.rm(
+        path.resolve(vitePath, "dist"),
+        { recursive: true, force: true },
+        () => null,
+      );
+    });
 
-  it("sets the correct bundle name", () => {
-    expect(stats.bundleName).toStrictEqual("vite-test-cjs");
+    it("sets the correct version", () => {
+      expect(stats.version).toStrictEqual(expectedV5Stats.version);
+    });
+
+    it("sets the correct plugin information", () => {
+      expect(stats.plugin).toStrictEqual(expectedV5Stats.plugin);
+    });
+
+    it("sets the correct bundler information", () => {
+      expect(stats.bundler).toStrictEqual(expectedV5Stats.bundler);
+    });
+
+    it("sets the correct bundle name", () => {
+      expect(stats.bundleName).toStrictEqual("vite-test-cjs");
+    });
   });
 });
