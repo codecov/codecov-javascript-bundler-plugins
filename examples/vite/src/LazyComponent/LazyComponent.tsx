e9635031fbe5b8a39d8572daa376397275a38909
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-25 13:42:39 -0400

    fix the vite app

--- examples/vite/src/LazyComponent/LazyComponent.tsx
+++ examples/vite/src/LazyComponent/LazyComponent.tsx
@@ -1,14 +1,7 @@
-import { lazy, Suspense } from "react";
-
-const IndexedLazyComponent = lazy(() => import("../IndexedLazyComponent"));
-
 export default function LazyComponent() {
   return (
     <div>
       <h1>Lazy Component</h1>
-      <Suspense fallback={null}>
-        <IndexedLazyComponent />
-      </Suspense>
     </div>
   );
 }
