f65cabeace8a39ee935b68d3882b4df5879c0993
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-26 08:43:47 -0400

    add some extra text to vite example app

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React - Codecov Demo Bundle Analysis Vite App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
