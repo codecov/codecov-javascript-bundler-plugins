365f7bb3f09a34c4a2fdc5b43d5c456e5f603769
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-29 13:48:44 -0400

    bring back indexed lazy component

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const IndexedLazyComponent = lazy(() => import("./IndexedLazyComponent"));
 const LazyComponent = lazy(() => import("./LazyComponent/LazyComponent"));
 
 function App() {
@@ -18,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React - Codecov Demo Bundle Analysis Vite App</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,6 +31,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Suspense fallback={null}>
+        <IndexedLazyComponent />
+      </Suspense>
       <Suspense fallback={null}>
         <LazyComponent />
       </Suspense>
