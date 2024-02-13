f3ad01677a81b480e29c350895faee0a2fc6a74d
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-02-13 10:35:34 -0400

    add bundler plugin metrics to plugin factory

--- packages/bundler-plugin-core/src/bundle-analysis/bundleAnalysisPluginFactory.ts
+++ packages/bundler-plugin-core/src/bundle-analysis/bundleAnalysisPluginFactory.ts
@@ -10,15 +10,18 @@ import { type NormalizedOptions } from "../utils/normalizeOptions.ts";
 import { detectProvider } from "../utils/provider.ts";
 import { uploadStats } from "../utils/uploadStats.ts";
 import { sendSentryBundleStats } from "../utils/sentryUtils.ts";
+import { type SentryClient } from "../sentry.ts";
 
 interface BundleAnalysisUploadPluginArgs {
   options: NormalizedOptions;
   bundleAnalysisUploadPlugin: BundleAnalysisUploadPlugin;
+  sentryClient: SentryClient;
 }
 
 export const bundleAnalysisPluginFactory = ({
   options,
   bundleAnalysisUploadPlugin,
+  sentryClient,
 }: BundleAnalysisUploadPluginArgs): UnpluginOptions => {
   const output: Output = {
     version: "1",
@@ -66,26 +69,65 @@ export const bundleAnalysisPluginFactory = ({
       const inputs: ProviderUtilInputs = { envs, args };
       const provider = await detectProvider(inputs);
 
+      const getPreSignedURLStart = Date.now();
       let url = "";
       try {
         url = await getPreSignedURL({
-          apiURL: options?.apiUrl ?? "https://api.codecov.io",
+          apiURL: options?.apiUrl,
           uploadToken: options?.uploadToken,
           serviceParams: provider,
           retryCount: options?.retryCount,
         });
+        sentryClient?.metricsAggregator?.add(
+          "c",
+          "request_presigned_url.success",
+          1,
+        );
       } catch (error) {
+        sentryClient?.metricsAggregator?.add(
+          "c",
+          "request_presigned_url.error",
+          1,
+        );
         return;
+      } finally {
+        sentryClient?.metricsAggregator?.add(
+          "d",
+          "request_presigned_url",
+          Date.now() - getPreSignedURLStart,
+          "millisecond",
+        );
       }
 
+      const uploadStart = Date.now();
       try {
         await uploadStats({
           preSignedUrl: url,
           bundleName: output.bundleName,
           message: JSON.stringify(output),
           retryCount: options?.retryCount,
+          sentryClient,
         });
-      } catch {}
+        sentryClient?.metricsAggregator?.add(
+          "c",
+          "upload_bundle_stats.success",
+          1,
+        );
+      } catch {
+        sentryClient?.metricsAggregator?.add(
+          "c",
+          "upload_bundle_stats.error",
+          1,
+        );
+        return;
+      } finally {
+        sentryClient?.metricsAggregator?.add(
+          "d",
+          "upload_bundle_stats",
+          Date.now() - uploadStart,
+          "millisecond",
+        );
+      }
     },
   };
 };
