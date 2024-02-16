a50562fd51180dfbb8ee41e421ab50a9e620a3d1
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-02-16 17:01:50 -0400

    fix type error

--- packages/bundler-plugin-core/src/bundle-analysis/__tests__/bundleAnalysisPluginFactory.test.ts
+++ packages/bundler-plugin-core/src/bundle-analysis/__tests__/bundleAnalysisPluginFactory.test.ts
@@ -18,7 +18,7 @@ describe("bundleAnalysisPluginFactory", () => {
         pluginVersion: "1.0.0",
       }),
       unpluginMetaContext: {} as UnpluginContextMeta,
-      sentryClient: undefined,
+      sentryMetrics: undefined,
       handleRecoverableError() {
         return;
       },
