61175c12b74f3daea2098ad170830bb77680b783
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-02-20 11:50:34 -0400

    update tests to hopefully work properly

--- integration-tests/fixtures/generate-bundle-stats/vite/vite-plugin.test.ts
+++ integration-tests/fixtures/generate-bundle-stats/vite/vite-plugin.test.ts
@@ -1,10 +1,12 @@
 import { $ } from "bun";
 import { describe, it, expect } from "bun:test";
-import { configV4 } from "./vite-v4-config";
-import { configV5 } from "./vite-v5-config";
+import { configV4 } from "./vite-v4-config-template";
+import { configV5 } from "./vite-v5-config-template";
 
-const vitePath = "node_modules/viteV4/bin/vite.js";
-const viteConfig = "fixtures/generate-bundle-stats/vite/vite-v4.config.ts";
+const vitePath = (version: number) =>
+  `node_modules/viteV${version}/bin/vite.js`;
+const viteConfig = (version: number) =>
+  `fixtures/generate-bundle-stats/vite/vite-v${version}.config.ts`;
 const viteApp = "test-apps/vite";
 
 describe("Generating vite stats", () => {
@@ -16,16 +18,26 @@ describe("Generating vite stats", () => {
         status: 200,
       });
 
-      await $`echo ${config} > ${viteConfig}`;
-      await $`node ${vitePath} build -c ${viteConfig}`;
+      const vite = vitePath(4);
+      const configFile = viteConfig(4);
+
+      await $`echo ${config} > ${configFile}`;
+      await $`node ${vite} build -c ${configFile}`;
 
       const res = await fetch(`http://localhost:8000/get-stats/${id}`);
-      const data = (await res.json()) as unknown;
-      // eslint-disable-next-line no-console
+      const data = (await res.json()) as { stats: string };
+      const stats = JSON.parse(data.stats) as unknown;
 
-      expect(data).toMatchSnapshot();
+      expect(stats).toMatchSnapshot({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        builtAt: expect.any(Number),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        duration: expect.any(Number),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        outputPath: expect.stringContaining("/distV4"),
+      });
 
-      await $`rm ${viteConfig}`;
+      await $`rm ${configFile}`;
       await $`rm -rf ${viteApp}/distV4`;
     });
   });
@@ -38,16 +50,26 @@ describe("Generating vite stats", () => {
         status: 200,
       });
 
-      await $`echo ${config} > ${viteConfig}`;
-      await $`node ${vitePath} build -c ${viteConfig}`;
+      const vite = vitePath(5);
+      const configFile = viteConfig(5);
+
+      await $`echo ${config} > ${configFile}`;
+      await $`node ${vite} build -c ${configFile}`;
 
       const res = await fetch(`http://localhost:8000/get-stats/${id}`);
-      const data = (await res.json()) as unknown;
-      // eslint-disable-next-line no-console
+      const data = (await res.json()) as { stats: string };
+      const stats = JSON.parse(data.stats) as unknown;
 
-      expect(data).toMatchSnapshot();
+      expect(stats).toMatchSnapshot({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        builtAt: expect.any(Number),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        duration: expect.any(Number),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        outputPath: expect.stringContaining("/distV5"),
+      });
 
-      await $`rm ${viteConfig}`;
+      await $`rm ${configFile}`;
       await $`rm -rf ${viteApp}/distV5`;
     });
   });
