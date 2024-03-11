146db0796ce0517f8c6a427a276029b29dfeead2
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-03-11 15:02:36 -0300

    setup JSDoc comments to work properly so we have some docs appear

--- packages/rollup-plugin/src/index.ts
+++ packages/rollup-plugin/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   codecovUnpluginFactory,
   type Options,
@@ -9,6 +10,30 @@ const codecovUnplugin = codecovUnpluginFactory({
   bundleAnalysisUploadPlugin: rollupBundleAnalysisPlugin,
 });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line isaacscript/complete-sentences-jsdoc
+/**
+ * Details for the Codecov Rollup plugin.
+ *
+ * @param {Options} options - See {@link @codecov/bundler-plugin-core!Options | Options} for more
+ *        details.
+ *
+ * @example
+ * ```typescript
+ * // rollup.config.js
+ * import { defineConfig } from "rollup";
+ * import { codecovRollupPlugin } from "@codecov/rollup-plugin";
+ *
+ * export default defineConfig({
+ *   plugins: [
+ *     // Put the Codecov rollup plugin after all other plugins
+ *     codecovRollupPlugin({
+ *       enableBundleAnalysis: true,
+ *       bundleName: "example-rollup-bundle",
+ *       uploadToken: process.env.CODECOV_TOKEN,
+ *     }),
+ *   ],
+ * });
+ * ```
+ */
 export const codecovRollupPlugin: (options: Options) => any =
   codecovUnplugin.rollup;
--- packages/webpack-plugin/src/index.ts
+++ packages/webpack-plugin/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   codecovUnpluginFactory,
   type Options,
@@ -9,6 +10,34 @@ const codecovUnplugin = codecovUnpluginFactory({
   bundleAnalysisUploadPlugin: webpackBundleAnalysisPlugin,
 });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/**
+ * Details for the Codecov Webpack plugin.
+ *
+ * @example
+ * ```typescript
+ * // webpack.config.js
+ * const path = require("path");
+ * const { codecovWebpackPlugin } = require("@codecov/webpack-plugin");
+ *
+ * module.exports = {
+ *   entry: "./src/index.js",
+ *   mode: "production",
+ *   output: {
+ *     filename: "main.js",
+ *     path: path.resolve(__dirname, "dist"),
+ *   },
+ *   plugins: [
+ *     // Put the Codecov vite plugin after all other plugins
+ *     codecovWebpackPlugin({
+ *       enableBundleAnalysis: true,
+ *       bundleName: "example-webpack-bundle",
+ *       uploadToken: process.env.CODECOV_TOKEN,
+ *     }),
+ *    ],
+ * };
+ * ```
+ *
+ * @see {@link @codecov/bundler-plugin-core!Options | Options} for list of options.
+ */
 export const codecovWebpackPlugin: (options: Options) => any =
   codecovUnplugin.webpack;
