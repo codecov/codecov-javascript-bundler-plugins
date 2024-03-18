ece4660ad69f9379526e60f0534199dfd196bf4b
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-03-18 07:58:27 -0300

    update rollup integration tests to use new gen script

--- integration-tests/fixtures/generate-bundle-stats/rollup/rollup-plugin.test.ts
+++ integration-tests/fixtures/generate-bundle-stats/rollup/rollup-plugin.test.ts
@@ -1,66 +1,59 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { $ } from "bun";
-import { describe, it, expect, afterEach } from "bun:test";
+import { describe, it, expect, afterEach, beforeEach } from "bun:test";
 
 const rollupPath = (version: number) =>
   `node_modules/rollupV${version}/dist/bin/rollup`;
-const rollupConfig = (version: number) =>
-  `fixtures/generate-bundle-stats/rollup/rollup-v${version}.config.cjs`;
+const rollupConfig = (version: number, format: string) =>
+  `fixtures/generate-bundle-stats/rollup/rollup-v${version}-${format}.config.cjs`;
 const rollupApp = "test-apps/rollup";
+const genConfigScript = "scripts/gen-config.sh";
+
+const VERSIONS = [3, 4];
+const FORMATS = [
+  "amd",
+  "cjs",
+  "es",
+  "esm",
+  "module",
+  "iife",
+  "umd",
+  "system",
+  "systemjs",
+];
 
 describe("Generating rollup stats", () => {
-  describe("version 3", () => {
-    afterEach(async () => {
-      await $`rm -rf ${rollupApp}/distV3`;
-    });
-
-    it("matches the snapshot", async () => {
-      const id = `rollup-v3-${Date.now()}`;
-      const rollup = rollupPath(3);
-      const configFile = rollupConfig(3);
-      const API_URL = `http://localhost:8000/test-url/${id}/200/false`;
-
-      // build the app
-      await $`API_URL=${API_URL} node ${rollup} -c ${configFile}`;
-
-      // fetch stats from the server
-      const res = await fetch(`http://localhost:8000/get-stats/${id}`);
-      const data = (await res.json()) as { stats: string };
-      const stats = JSON.parse(data.stats) as unknown;
-
-      // assert the stats
-      expect(stats).toMatchSnapshot({
-        builtAt: expect.any(Number),
-        duration: expect.any(Number),
-        outputPath: expect.stringContaining("/distV3"),
+  describe.each(VERSIONS)("%d", (version) => {
+    describe.each(FORMATS)("%s", (format) => {
+      beforeEach(async () => {
+        await $`sh ${genConfigScript} rollup esm ${format} v3 v${version} cjs`;
       });
-    });
-  });
 
-  describe("version 4", () => {
-    afterEach(async () => {
-      await $`rm -rf ${rollupApp}/distV4`;
-    });
+      afterEach(async () => {
+        await $`rm -rf ${rollupConfig(version, format)}`;
+        await $`rm -rf ${rollupApp}/distV${version}`;
+      });
 
-    it("matches the snapshot", async () => {
-      const id = `rollup-v4-${Date.now()}`;
-      const rollup = rollupPath(4);
-      const configFile = rollupConfig(4);
-      const API_URL = `http://localhost:8000/test-url/${id}/200/false`;
+      it("matches the snapshot", async () => {
+        const id = `rollup-v${version}-${format}-${Date.now()}`;
+        const rollup = rollupPath(version);
+        const configFile = rollupConfig(version, format);
+        const API_URL = `http://localhost:8000/test-url/${id}/200/false`;
 
-      // build the app
-      await $`API_URL=${API_URL} node ${rollup} -c ${configFile}`;
+        // build the app
+        await $`API_URL=${API_URL} node ${rollup} -c ${configFile}`;
 
-      // fetch stats from the server
-      const res = await fetch(`http://localhost:8000/get-stats/${id}`);
-      const data = (await res.json()) as { stats: string };
-      const stats = JSON.parse(data.stats) as unknown;
+        // fetch stats from the server
+        const res = await fetch(`http://localhost:8000/get-stats/${id}`);
+        const data = (await res.json()) as { stats: string };
+        const stats = JSON.parse(data.stats) as unknown;
 
-      // assert the stats
-      expect(stats).toMatchSnapshot({
-        builtAt: expect.any(Number),
-        duration: expect.any(Number),
-        outputPath: expect.stringContaining("/distV4"),
+        // assert the stats
+        expect(stats).toMatchSnapshot({
+          builtAt: expect.any(Number),
+          duration: expect.any(Number),
+          outputPath: expect.stringContaining(`/distV${version}`),
+        });
       });
     });
   });
