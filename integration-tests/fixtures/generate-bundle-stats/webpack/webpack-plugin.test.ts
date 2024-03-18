e7bba6da666c3441c3f82ffe5ab95a1677a2a333
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-03-18 07:58:51 -0300

    update webpack integration tests to use new gen script

--- integration-tests/fixtures/generate-bundle-stats/webpack/webpack-plugin.test.ts
+++ integration-tests/fixtures/generate-bundle-stats/webpack/webpack-plugin.test.ts
@@ -1,37 +1,48 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { $ } from "bun";
-import { describe, it, expect, afterEach } from "bun:test";
+import { describe, it, expect, afterEach, beforeEach } from "bun:test";
 
 const webpackPath = (version: number) =>
   `node_modules/webpackV${version}/bin/webpack.js`;
-const webpackConfig = (version: number) =>
-  `fixtures/generate-bundle-stats/webpack/webpack-v${version}.config.cjs`;
-const webpackApp = "test-apps/vite";
+const webpackConfig = (version: number, format: string) =>
+  `fixtures/generate-bundle-stats/webpack/webpack-v${version}-${format}.config.cjs`;
+const webpackApp = "test-apps/webpack";
+const genConfigScript = "scripts/gen-config.sh";
+
+const VERSIONS = [5];
+const FORMATS = ["array-push", "commonjs", "module"];
 
 describe("Generating webpack stats", () => {
-  describe("version 5", () => {
-    afterEach(async () => {
-      await $`rm -rf ${webpackApp}/dist`;
-    });
+  describe.each(VERSIONS)(`%d`, (version) => {
+    describe.each(FORMATS)(`%s`, (format) => {
+      beforeEach(async () => {
+        await $`sh ${genConfigScript} webpack commonjs ${format} v5 v${version} cjs`;
+      });
 
-    it("matches the snapshot", async () => {
-      const id = `webpack-v5-${Date.now()}`;
-      const webpack = webpackPath(5);
-      const configFile = webpackConfig(5);
-      const API_URL = `http://localhost:8000/test-url/${id}/200/false`;
+      afterEach(async () => {
+        await $`rm -rf ${webpackConfig(version, format)}`;
+        await $`rm -rf ${webpackApp}/distV${version}`;
+      });
 
-      // build the app
-      await $`API_URL=${API_URL} node ${webpack} --config ${configFile}`;
+      it("matches the snapshot", async () => {
+        const id = `webpack-v${version}-${format}-${Date.now()}`;
+        const webpack = webpackPath(version);
+        const configFile = webpackConfig(version, format);
+        const API_URL = `http://localhost:8000/test-url/${id}/200/false`;
 
-      // fetch stats from the server
-      const res = await fetch(`http://localhost:8000/get-stats/${id}`);
-      const data = (await res.json()) as { stats: string };
-      const stats = JSON.parse(data.stats) as unknown;
+        // build the app
+        await $`API_URL=${API_URL} node ${webpack} --config ${configFile}`;
 
-      expect(stats).toMatchSnapshot({
-        builtAt: expect.any(Number),
-        duration: expect.any(Number),
-        outputPath: expect.stringContaining("/dist"),
+        // fetch stats from the server
+        const res = await fetch(`http://localhost:8000/get-stats/${id}`);
+        const data = (await res.json()) as { stats: string };
+        const stats = JSON.parse(data.stats) as unknown;
+
+        expect(stats).toMatchSnapshot({
+          builtAt: expect.any(Number),
+          duration: expect.any(Number),
+          outputPath: expect.stringContaining(`/distV${version}`),
+        });
       });
     });
   });
