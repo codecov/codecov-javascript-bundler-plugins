85226eddc946f742e4b9959e40ab18dd7d5729b5
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-04-05 10:07:54 -0300

    add in new column, and endpoint to grab stats via the bundle name

--- integration-tests/test-api/src/index.ts
+++ integration-tests/test-api/src/index.ts
@@ -7,7 +7,7 @@ console.log("connected to sqlite");
 
 console.log('creating "stats" table');
 const query = sqlite.query(
-  "CREATE TABLE `stats` (`id` text, `json_stats` text );",
+  "CREATE TABLE `stats` (`id` text, `bundle_name` text, `json_stats` text );",
 );
 query.run();
 console.log('created "stats" table');
@@ -52,13 +52,14 @@ app.all("/file-upload/:id/:status{[0-9]{3}}", async (c) => {
   }
 
   console.log("uploading file");
-  const data: unknown = await c.req.json();
+  const data: { bundleName: string } = await c.req.json();
   console.log("finished upload");
 
   console.log("inserting stats");
+  const bundleName = data!.bundleName;
   const insertStats = JSON.stringify(data);
   const query = sqlite.query(
-    `INSERT INTO stats (id, json_stats) VALUES ('${id}', '${insertStats}')`,
+    `INSERT INTO stats (id, bundle_name, json_stats) VALUES ('${id}', '${bundleName}', '${insertStats}')`,
   );
   query.run();
   query.finalize();
@@ -84,6 +85,33 @@ app.all("/get-stats/:id", (c) => {
   }
 
   console.log("stats not found", id);
+  return c.text("Not found", { status: 404 });
+});
+
+app.all("/get-stats-by-bundle-name/:bundleName", (c) => {
+  const bundleName = c.req.param("bundleName");
+  console.log("getting stats", bundleName);
+
+  const query = sqlite.query(
+    "SELECT * FROM stats WHERE bundle_name = $bundleName",
+  );
+  const result = query.get({ $bundleName: bundleName }) as {
+    id: string;
+    json_stats: string;
+  };
+  query.finalize();
+
+  if (result) {
+    console.log("stats found", bundleName);
+    const query = sqlite.query(
+      `DELETE FROM stats WHERE bundle_name = '${bundleName}'`,
+    );
+    query.run();
+    query.finalize();
+    return c.json({ stats: result.json_stats }, { status: 200 });
+  }
+
+  console.log("stats not found", bundleName);
   return c.text("Not found", { status: 404 });
 });
 
