46b68ca7673b1e4b1922fc0efb215a81a9491051
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-04-22 07:48:25 -0300

    small update to bundler plugin tests

--- packages/rollup-plugin/src/rollup-bundle-analysis/__tests__/rollupBundleAnalysisPlugin.test.ts
+++ packages/rollup-plugin/src/rollup-bundle-analysis/__tests__/rollupBundleAnalysisPlugin.test.ts
@@ -1,20 +1,19 @@
+import { Output } from "@codecov/bundler-plugin-core";
 import { rollupBundleAnalysisPlugin } from "../rollupBundleAnalysisPlugin";
 
 describe("rollupBundleAnalysisPlugin", () => {
   describe("when called", () => {
     it("returns a plugin object", () => {
       const plugin = rollupBundleAnalysisPlugin({
-        output: {
-          bundleName: "test",
-        },
-        options: {
-          bundleName: "test",
+        output: new Output({
           apiUrl: "http://localhost",
+          bundleName: "test-bundle",
+          debug: false,
           dryRun: true,
           enableBundleAnalysis: true,
           retryCount: 1,
-          debug: false,
-        },
+          uploadToken: "test-token",
+        }),
       });
 
       expect(plugin).toMatchSnapshot();
--- packages/webpack-plugin/src/webpack-bundle-analysis/__tests__/webpackBundleAnalysisPlugin.test.ts
+++ packages/webpack-plugin/src/webpack-bundle-analysis/__tests__/webpackBundleAnalysisPlugin.test.ts
@@ -1,20 +1,19 @@
+import { Output } from "@codecov/bundler-plugin-core";
 import { webpackBundleAnalysisPlugin } from "../webpackBundleAnalysisPlugin";
 
 describe("webpackBundleAnalysisPlugin", () => {
   describe("when called", () => {
     it("returns a plugin object", () => {
       const plugin = webpackBundleAnalysisPlugin({
-        output: {
-          bundleName: "test",
-        },
-        options: {
-          bundleName: "test",
+        output: new Output({
           apiUrl: "http://localhost",
+          bundleName: "test-bundle",
+          debug: false,
           dryRun: true,
           enableBundleAnalysis: true,
           retryCount: 1,
-          debug: false,
-        },
+          uploadToken: "test-token",
+        }),
       });
 
       expect(plugin).toMatchSnapshot();
