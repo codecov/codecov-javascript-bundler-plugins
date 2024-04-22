6645aab1445bed6424e3b3748c68f720c957fb1b
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-04-22 14:50:47 -0300

    setup testing nuxt example

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -1,4 +1,14 @@
+import { codecovNuxtPlugin } from "@codecov/nuxt-plugin";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  vite: {
+    plugins: [
+      codecovNuxtPlugin({
+        dryRun: true,
+        bundleName: "nuxt-bundle-analysis",
+        enableBundleAnalysis: true,
+      }),
+    ],
+  },
 });
