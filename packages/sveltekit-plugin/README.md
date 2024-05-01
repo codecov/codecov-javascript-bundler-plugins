fcb8955041e295f5731e68cd34f4c65c5e83eb48
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-05-01 09:23:00 -0300

    add in new sveltekit plugin

---
+++ packages/sveltekit-plugin/README.md
@@ -0,0 +1,64 @@
+<p align="center">
+  <a href="https://about.codecov.io" target="_blank">
+    <img src="https://about.codecov.io/wp-content/themes/codecov/assets/brand/sentry-cobranding/logos/codecov-by-sentry-logo.svg" alt="Codecov by Sentry logo" width="280" height="84">
+  </a>
+</p>
+
+# Codecov SvelteKit Plugin
+
+> [!WARNING]
+> These plugins are currently in beta and are subject to change.
+>
+> A SvelteKit plugin that provides bundle analysis support for Codecov.
+>
+> The plugin does not support code coverage, see our [docs](https://docs.codecov.com/docs/quick-start) to set up coverage today!
+
+> [!NOTE]
+> This plugin only support SvelteKit 2.x when building with Vite.
+
+## Installation
+
+Using npm:
+
+```bash
+npm install @codecov/sveltekit-plugin --save-dev
+```
+
+Using yarn:
+
+```bash
+yarn add @codecov/sveltekit-plugin --dev
+```
+
+Using pnpm:
+
+```bash
+pnpm add @codecov/sveltekit-plugin --save-dev
+```
+
+## Example
+
+```ts
+// vite.config.ts
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { codecovSvelteKitPlugin } from "@codecov/sveltekit-plugin";
+
+export default defineConfig({
+  plugins: [
+    sveltekit(),
+    // Put the Codecov SvelteKit plugin after all other plugins
+    codecovSvelteKitPlugin({
+      enableBundleAnalysis: true,
+      bundleName: "example-sveltekit-bundle",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
+});
+```
+
+## More information
+
+- [Codecov Documentation](https://docs.codecov.com/docs)
+- [Codecov Feedback](https://github.com/codecov/feedback/discussions)
+- [Sentry Discord](https://discord.gg/Ww9hbqr)
---
+++ packages/sveltekit-plugin/src/index.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  type UnpluginOptions,
+  createVitePlugin,
+  type VitePlugin,
+} from "unplugin";
+import {
+  type Options,
+  normalizeOptions,
+  red,
+  checkNodeVersion,
+  Output,
+} from "@codecov/bundler-plugin-core";
+import { _internal_viteBundleAnalysisPlugin } from "@codecov/vite-plugin";
+
+import { sveltekitBundleAnalysisPlugin } from "./sveltekit-bundle-analysis/sveltekitBundleAnalysisPlugin";
+
+const codecovSvelteKitPluginFactory = createVitePlugin<Options, true>(
+  (userOptions, unpluginMetaContext) => {
+    if (checkNodeVersion(unpluginMetaContext)) {
+      return [];
+    }
+
+    const normalizedOptions = normalizeOptions(userOptions);
+    if (!normalizedOptions.success) {
+      for (const error of normalizedOptions.errors) {
+        red(error);
+      }
+      return [];
+    }
+
+    const plugins: UnpluginOptions[] = [];
+    const output = new Output(normalizedOptions.options);
+    const options = normalizedOptions.options;
+    if (options.enableBundleAnalysis) {
+      plugins.push(
+        sveltekitBundleAnalysisPlugin({ output }),
+        _internal_viteBundleAnalysisPlugin({ output }),
+      );
+    }
+
+    return plugins;
+  },
+);
+
+/**
+ * Details for the Codecov SvelteKit plugin.
+ *
+ * @example
+ * ```typescript
+ * // vite.config.ts
+ * import { sveltekit } from "@sveltejs/kit/vite";
+ * import { defineConfig } from "vite";
+ * import { codecovSvelteKitPlugin } from "@codecov/sveltekit-plugin";
+ *
+ * export default defineConfig({
+ *   plugins: [
+ *     sveltekit(),
+ *      // Put the Codecov SvelteKit plugin after all other plugins
+ *     codecovSvelteKitPlugin({
+ *       enableBundleAnalysis: true,
+ *       bundleName: "example-sveltekit-bundle",
+ *       uploadToken: process.env.CODECOV_TOKEN,
+ *     }),
+ *   ],
+ * });
+ *
+ * ```
+ *
+ * @see {@link @codecov/bundler-plugin-core!Options | Options} for list of options.
+ */
+export const codecovSvelteKitPlugin: (options: Options) => VitePlugin<any>[] =
+  codecovSvelteKitPluginFactory;
