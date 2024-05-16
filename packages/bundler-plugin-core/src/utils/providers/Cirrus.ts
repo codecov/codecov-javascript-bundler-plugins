55aad733bd39bedddf8b223c93a2c435f76d1bbb
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-05-16 10:18:18 -0300

    add in sha debug logging to Cirrus

--- packages/bundler-plugin-core/src/utils/providers/Cirrus.ts
+++ packages/bundler-plugin-core/src/utils/providers/Cirrus.ts
@@ -3,6 +3,8 @@ import {
   type ProviderServiceParams,
   type ProviderUtilInputs,
 } from "../../types.ts";
+import { type Output } from "../Output.ts";
+import { debug } from "../logging.ts";
 import { setSlug } from "../provider.ts";
 
 export function detect(envs: ProviderEnvs): boolean {
@@ -11,7 +13,10 @@ export function detect(envs: ProviderEnvs): boolean {
 
 function _getBuild(inputs: ProviderUtilInputs): string {
   const { args, envs } = inputs;
-  return args?.build ?? envs?.CIRRUS_BUILD_ID ?? "";
+  if (args?.build && args.build !== "") {
+    return args.build;
+  }
+  return envs?.CIRRUS_BUILD_ID ?? "";
 }
 
 function _getBuildURL(): string {
@@ -20,7 +25,10 @@ function _getBuildURL(): string {
 
 function _getBranch(inputs: ProviderUtilInputs): string {
   const { args, envs } = inputs;
-  return args?.branch ?? envs?.CIRRUS_BRANCH ?? "";
+  if (args?.branch && args.branch !== "") {
+    return args.branch;
+  }
+  return envs?.CIRRUS_BRANCH ?? "";
 }
 
 function _getJob(envs: ProviderEnvs): string {
@@ -29,7 +37,10 @@ function _getJob(envs: ProviderEnvs): string {
 
 function _getPR(inputs: ProviderUtilInputs): string {
   const { args, envs } = inputs;
-  return args?.pr ?? envs?.CIRRUS_PR ?? "";
+  if (args?.pr && args.pr !== "") {
+    return args.pr;
+  }
+  return envs?.CIRRUS_PR ?? "";
 }
 
 function _getService(): string {
@@ -40,9 +51,15 @@ export function getServiceName(): string {
   return "Cirrus CI";
 }
 
-function _getSHA(inputs: ProviderUtilInputs): string {
+function _getSHA(inputs: ProviderUtilInputs, output: Output): string {
   const { args, envs } = inputs;
-  return args?.sha ?? envs?.CIRRUS_CHANGE_IN_REPO ?? "";
+  if (args?.sha && args.sha !== "") {
+    debug(`Using commit: ${args.sha}`, { enabled: output.debug });
+    return args.sha;
+  }
+  const sha = envs?.CIRRUS_CHANGE_IN_REPO ?? "";
+  debug(`Using commit: ${sha}`, { enabled: output.debug });
+  return sha;
 }
 
 function _getSlug(inputs: ProviderUtilInputs): string {
@@ -53,12 +70,13 @@ function _getSlug(inputs: ProviderUtilInputs): string {
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function getServiceParams(
   inputs: ProviderUtilInputs,
+  output: Output,
 ): Promise<ProviderServiceParams> {
   return {
     branch: _getBranch(inputs),
     build: _getBuild(inputs),
     buildURL: _getBuildURL(),
-    commit: _getSHA(inputs),
+    commit: _getSHA(inputs, output),
     job: _getJob(inputs.envs),
     pr: _getPR(inputs),
     service: _getService(),
--- packages/bundler-plugin-core/src/utils/providers/__tests__/Cirrus.test.ts
+++ packages/bundler-plugin-core/src/utils/providers/__tests__/Cirrus.test.ts
@@ -5,6 +5,7 @@ import {
   type ProviderServiceParams,
   type ProviderUtilInputs,
 } from "../../../types.ts";
+import { Output } from "../../Output.ts";
 import * as Cirrus from "../Cirrus.ts";
 
 describe("Cirrus Params", () => {
@@ -59,7 +60,16 @@ describe("Cirrus Params", () => {
       service: "cirrus-ci",
       slug: "testOrg/testRepo",
     };
-    const params = await Cirrus.getServiceParams(inputs);
+
+    const output = new Output({
+      apiUrl: "http://localhost",
+      bundleName: "Cirrus-test",
+      debug: false,
+      dryRun: true,
+      enableBundleAnalysis: true,
+      retryCount: 0,
+    });
+    const params = await Cirrus.getServiceParams(inputs, output);
     expect(params).toMatchObject(expected);
   });
 
@@ -97,7 +107,15 @@ describe("Cirrus Params", () => {
       slug: "testOrg/testRepo",
     };
 
-    const params = await Cirrus.getServiceParams(inputs);
+    const output = new Output({
+      apiUrl: "http://localhost",
+      bundleName: "Cirrus-test",
+      debug: false,
+      dryRun: true,
+      enableBundleAnalysis: true,
+      retryCount: 0,
+    });
+    const params = await Cirrus.getServiceParams(inputs, output);
     expect(params).toMatchObject(expected);
   });
 });
