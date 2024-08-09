a0b556fe32b29cd10bf6621f48a01f09067195ec
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-08-09 09:58:41 -0300

    add in new function used to find the origin git service

---
+++ packages/bundler-plugin-core/src/utils/findGitService.ts
@@ -0,0 +1,79 @@
+import { runExternalProgram } from "./runExternalProgram.ts";
+
+const GIT_SERVICES = [
+  "github",
+  "gitlab",
+  "bitbucket",
+  "github_enterprise",
+  "gitlab_enterprise",
+  "bitbucket_server",
+] as const;
+
+/**
+ * Possible cases we're considering:
+ *  - https://github.com/codecov/codecov-cli.git returns github
+ *  - git@github.com:codecov/codecov-cli.git returns github
+ *  - ssh://git@github.com/gitcodecov/codecov-cli returns github
+ *  - ssh://git@github.com:gitcodecov/codecov-cli returns github
+ *  - https://user-name@bitbucket.org/namespace-codecov/first_repo.git returns bitbucket
+ * Copied from: https://github.com/codecov/codecov-cli/blob/main/codecov_cli/helpers/git.py#L72-L91
+ */
+
+export const findGitService = () => {
+  const remotes = runExternalProgram("git", ["remote"]).split("\n");
+
+  const remoteName =
+    remotes.find((remote) => remote.includes("origin")) ?? remotes?.[0];
+
+  if (!remoteName || remoteName === "") {
+    throw new Error("No remote found");
+  }
+
+  const remoteService = runExternalProgram("git", [
+    "ls-remote",
+    "--get-url",
+    remoteName,
+  ]).trim();
+
+  return parseGitService(remoteService);
+};
+
+export const parseGitService = (gitService: string) => {
+  let validURL = false;
+  try {
+    new URL(gitService);
+    validURL = true;
+  } catch (e) {
+    validURL = false;
+  }
+
+  let service = "";
+
+  if (validURL) {
+    const parsedURL = new URL(gitService);
+    const { protocol } = parsedURL;
+    service = splitPath(gitService.replace(`${protocol}//`, ""));
+  } else if (gitService.startsWith("git@")) {
+    service = splitPath(gitService);
+  }
+
+  if (GIT_SERVICES.includes(service)) {
+    return service;
+  }
+  return;
+};
+
+export const splitPath = (path: string) => {
+  if (path.includes("@")) {
+    path = path.split("@", 2)[1] ?? path;
+  }
+
+  if (path.includes(":")) {
+    path = path.split(":", 1)[0] ?? path;
+  }
+
+  if (path.includes(".")) {
+    path = path.split(".", 1)[0] ?? path;
+  }
+  return path;
+};
