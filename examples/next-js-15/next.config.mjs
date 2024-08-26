0358a16efcfa60e29cfd07cddb2e494233e7cd36
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-08-26 12:26:21 -0300

    add in example nextjs 15 app

---
+++ examples/next-js-15/next.config.mjs
@@ -0,0 +1,22 @@
+import { codecovNextJSWebpackPlugin } from "@codecov/nextjs-webpack-plugin";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, options) => {
+    config.plugins.push(
+      codecovNextJSWebpackPlugin({
+        enableBundleAnalysis: true,
+        bundleName: "@codecov/example-next-15-app",
+        uploadToken: process.env.NEXT_UPLOAD_TOKEN,
+        apiUrl: process.env.NEXT_API_URL,
+        webpack: options.webpack,
+        debug: true,
+        dryRun: true,
+      }),
+    );
+
+    return config;
+  },
+};
+
+export default nextConfig;
