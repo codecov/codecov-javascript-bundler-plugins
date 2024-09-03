35cbb28f4418c13f90f1aec839bcb044da8628c9
Rohit <rohit.vinnakota@sentry.io>
2024-09-03 13:45:12 -0400

    initial commit

--- integration-tests/fixtures/generate-bundle-stats/sveltekit/vite-base.config.ts
+++ integration-tests/fixtures/generate-bundle-stats/sveltekit/vite-base.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    //@ts-expect-error handle conflicting vite version types
     sveltekit(),
     codecovSvelteKitPlugin({
       enableBundleAnalysis: true,
