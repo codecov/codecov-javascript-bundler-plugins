c972fddd595b56722906fd319768e3f4bee50e75
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-09-24 16:13:11 -0400

    remove debug log from imports

--- packages/bundler-plugin-core/src/utils/getPreSignedURL.ts
+++ packages/bundler-plugin-core/src/utils/getPreSignedURL.ts
@@ -4,7 +4,7 @@ import { FailedFetchError } from "../errors/FailedFetchError.ts";
 import { UploadLimitReachedError } from "../errors/UploadLimitReachedError.ts";
 import { type ProviderServiceParams } from "../types.ts";
 import { fetchWithRetry } from "./fetchWithRetry.ts";
-import { debug, green, red } from "./logging.ts";
+import { green, red } from "./logging.ts";
 import { preProcessBody } from "./preProcessBody.ts";
 import { NoUploadTokenError } from "../errors/NoUploadTokenError.ts";
 import { findGitService } from "./findGitService.ts";
