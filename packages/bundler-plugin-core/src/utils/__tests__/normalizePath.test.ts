005813c702dad233e1ea835d2f8b4dc168923700
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-10-18 12:29:00 -0300

    update tests

--- packages/bundler-plugin-core/src/utils/__tests__/normalizePath.test.ts
+++ packages/bundler-plugin-core/src/utils/__tests__/normalizePath.test.ts
@@ -83,6 +83,38 @@ const tests: Test[] = [
     },
     expected: "test.*",
   },
+  {
+    name: "should replace '[hash:22]' with '*'",
+    input: {
+      path: "test.123.chunk.js",
+      format: "[name].[hash:22].chunk.js",
+    },
+    expected: "test.*.chunk.js",
+  },
+  {
+    name: "should replace '[contenthash:22]' with '*'",
+    input: {
+      path: "test.123.chunk.js",
+      format: "[name].[contenthash:22].chunk.js",
+    },
+    expected: "test.*.chunk.js",
+  },
+  {
+    name: "should replace '[fullhash:22]' with '*'",
+    input: {
+      path: "test.123.chunk.js",
+      format: "[name].[fullhash:22].chunk.js",
+    },
+    expected: "test.*.chunk.js",
+  },
+  {
+    name: "should replace '[chunkhash:22]' with '*'",
+    input: {
+      path: "test.123.chunk.js",
+      format: "[name].[chunkhash:22].chunk.js",
+    },
+    expected: "test.*.chunk.js",
+  },
 ];
 
 describe("normalizePath", () => {
