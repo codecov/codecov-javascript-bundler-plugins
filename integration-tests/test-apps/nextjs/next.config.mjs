87d660faf6915b168cbbfe6a72e6e5aef2aa48b3
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-10-29 07:16:39 -0300

    didn't have correct bundle name to find

--- integration-tests/test-apps/nextjs/next.config.mjs
+++ integration-tests/test-apps/nextjs/next.config.mjs
@@ -2,14 +2,11 @@ import { codecovNextJSWebpackPlugin } from "@codecov/nextjs-webpack-plugin";
 
 /** @type {import('next').NextConfig} */
 export default {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   webpack: (config, options) => {
     config.plugins.push(
       codecovNextJSWebpackPlugin({
         enableBundleAnalysis: true,
-        bundleName: "",
+        bundleName: "test-nextjs-v15",
         uploadToken: "test-token",
         apiUrl: process.env.API_URL,
         webpack: options.webpack,
