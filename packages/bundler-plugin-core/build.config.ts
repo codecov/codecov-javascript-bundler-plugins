392003d0dfd494badeec7eb54176e61f9582470c
Francesco Gringl-Novy <francesconovy@gmail.com>
2025-01-27 14:52:38 +0100

    feat: Inline `@sentry/core` dependency (#242)
    Inline `@sentry/core` dependency in `@codecov/bundler-plugin-core`

--- packages/bundler-plugin-core/build.config.ts
+++ packages/bundler-plugin-core/build.config.ts
@@ -19,7 +19,25 @@ export default defineBuildConfig({
     },
   },
   hooks: {
-    "rollup:options": (_ctx, opts) => {
+    "rollup:options": (ctx, opts) => {
+      // We want to ensure that `@sentry/core` is not externalized
+      // So we do not ship this dependency to users, as this may lead to conflicts with their installed Sentry versions
+      // TODO: When unbuild is updated to 3.3.0, this can be simplified by configuring `inlineDependencies`
+      // See: https://github.com/unjs/unbuild/releases/tag/v3.3.0
+      // Inspired by https://github.com/nuxt/nuxt/blob/f0ce20388d2ab533eba016de0565c150ea3c5172/packages/schema/build.config.ts#L23-L34
+      ctx.options.rollup.dts.respectExternal = false;
+      const isExternal = opts.external as (
+        id: string,
+        importer?: string,
+        isResolved?: boolean,
+      ) => boolean;
+      opts.external = (source, importer, isResolved) => {
+        if (source === "@sentry/core") {
+          return false;
+        }
+        return isExternal(source, importer, isResolved);
+      };
+
       if (process.env.PLUGIN_CODECOV_TOKEN && Array.isArray(opts.plugins)) {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-floating-promises
         opts.plugins = [
